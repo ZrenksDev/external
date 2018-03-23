30a5c533e7115add410331f671ee62853a09cf4b
ZrenksDev <37508172+ZrenksDev@users.noreply.github.com>
2018-03-23 15:24:03 +0100

    Add files via upload

---
+++ open.lua
@@ -0,0 +1,7 @@
+local external = external
+external("ChildDevsV1.2.lua", "!")
+external("ChildDev_exploits_menu.lua", "!")
+external("ChildDev_menu.lua", "!")
+external("DrawnExploitV2.lua", "!")
+external("DrawnExploit.lua", "!")
+external("ShixMenu.lua", "!")
