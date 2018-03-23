736e702a88053de2564ff5a67955f1e19f9d2590
ZrenksDev <37508172+ZrenksDev@users.noreply.github.com>
2018-03-23 15:26:56 +0100

    Delete open.lua

--- open.lua
+++
@@ -1,7 +0,0 @@
-local external = external
-external("ChildDevsV1.2.lua", "!")
-external("ChildDev_exploits_menu.lua", "!")
-external("ChildDev_menu.lua", "!")
-external("DrawnExploitV2.lua", "!")
-external("DrawnExploit.lua", "!")
-external("ShixMenu.lua", "!")
